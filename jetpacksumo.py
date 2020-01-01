948af2a33f3147ec23cd6fcc739276d7b636e4dc
Ben Lee <benleemedia@gmail.com>
2020-01-01 18:31:06 -0500

    removed some redundant lines of code. removed the reset key moved keypress_detect() out of main moved check_player_collision() out of main moved draw_text() out of main

--- jetpacksumo.py
+++ jetpacksumo.py
@@ -12,7 +12,7 @@ import math
 from customclasses.ball import Ball
 from graphics.draw import draw_flames
 from graphics.draw import draw_arena
-from data.vardata import *
+from data.vardata import palette, gamevar
 
 def reset(ball_list, arena, score):
     for ball in ball_list:
@@ -66,6 +66,74 @@ def make_ball(color):
 
     return ball
 
+def keypress_detect(pygame, player_1, player_2, done):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                player_1.moveup = True
+            if event.key == pygame.K_a:
+                player_1.moveleft = True
+            if event.key == pygame.K_d:
+                player_1.moveright = True
+            if event.key == pygame.K_s:
+                player_1.movedown = True
+            if event.key == pygame.K_UP:
+                player_2.moveup = True
+            if event.key == pygame.K_LEFT:
+                player_2.moveleft = True
+            if event.key == pygame.K_RIGHT:
+                player_2.moveright = True
+            if event.key == pygame.K_DOWN:
+                player_2.movedown = True
+        #What happens when the key is released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                    player_1.moveup = False
+            if event.key == pygame.K_a:
+                    player_1.moveleft = False
+            if event.key == pygame.K_d:
+                    player_1.moveright = False
+            if event.key == pygame.K_s:
+                player_1.movedown = False
+            if event.key == pygame.K_UP:
+                    player_2.moveup = False
+            if event.key == pygame.K_LEFT:
+                    player_2.moveleft = False
+            if event.key == pygame.K_RIGHT:
+                    player_2.moveright = False
+            if event.key == pygame.K_DOWN:
+                player_2.movedown = False
+    return player_1, player_2, done
+
+def check_player_collisions(player_1, player_2):
+    if player_1.collision_detect_alt(player_2):
+        temp_speed_x = player_1.speed_x
+        temp_speed_y = player_1.speed_y
+        print("COLLISION!")
+        player_1.speed_x = player_2.speed_x*1.5
+        player_1.speed_y = player_2.speed_y*1.5
+        player_2.speed_x = temp_speed_x*1.5
+        player_2.speed_y = temp_speed_y*1.5
+        player_1.size += 1
+        player_2.size += 1
+    return player_1, player_2
+
+def draw_text(screen, text, text2, text3, arena):
+    textRect = text.get_rect()
+    textRect.center = (round(gamevar.screen_width/2),round(gamevar.screen_height/1.5))
+    textRect2 = text2.get_rect()
+    textRect2.center = (50, round(gamevar.screen_height/2))
+    textRect3 = text3.get_rect()
+    textRect3.center = (round(gamevar.screen_width -50), round(gamevar.screen_height/2))
+    screen = draw_arena(screen, arena, gamevar.screen_height, palette)
+    #draws the text onto the screen
+    screen.blit(text, textRect)
+    screen.blit(text2, textRect2)
+    screen.blit(text3, textRect3)
+    return screen
+
 def main():
     """
     This is our main program.
@@ -112,64 +180,10 @@ def main():
 
     # -------- Main Program Loop -----------
     while not done:
-        # --- Event Processing / Keypresses
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    player_1.moveup = True
-                if event.key == pygame.K_a:
-                    player_1.moveleft = True
-                if event.key == pygame.K_d:
-                    player_1.moveright = True
-                if event.key == pygame.K_s:
-                    player_1.movedown = True
-                if event.key == pygame.K_r:
-                    for player in player_list:
-                        ball_list, arena, text, text2, text3, winner= reset(ball_list, arena, score)
-                #Player 2 controls
-                if event.key == pygame.K_UP:
-                    player_2.moveup = True
-                if event.key == pygame.K_LEFT:
-                    player_2.moveleft = True
-                if event.key == pygame.K_RIGHT:
-                    player_2.moveright = True
-                if event.key == pygame.K_DOWN:
-                    player_2.movedown = True
-            #What happens when the key is released
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                        player_1.moveup = False
-                if event.key == pygame.K_a:
-                        player_1.moveleft = False
-                if event.key == pygame.K_d:
-                        player_1.moveright = False
-                if event.key == pygame.K_s:
-                    player_1.movedown = False
-                if event.key == pygame.K_UP:
-                        player_2.moveup = False
-                if event.key == pygame.K_LEFT:
-                        player_2.moveleft = False
-                if event.key == pygame.K_RIGHT:
-                        player_2.moveright = False
-                if event.key == pygame.K_DOWN:
-                    player_2.movedown = False
-                    #ball = make_ball()
-                    #ball_list.append(ball)
-
+        player_1, player_2, done = keypress_detect(pygame, player_1, player_2, done)
         # --- Logic
         #basically switches the player 1 and player 2 speeds in a messy way. Recode later
-        if player_1.collision_detect_alt(player_2):
-            temp_speed_x = player_1.speed_x
-            temp_speed_y = player_1.speed_y
-            print("COLLISION!")
-            player_1.speed_x = player_2.speed_x*1.5
-            player_1.speed_y = player_2.speed_y*1.5
-            player_2.speed_x = temp_speed_x*1.5
-            player_2.speed_y = temp_speed_y*1.5
-            player_1.size += 1
-            player_2.size += 1
+        player_1, player_2 = check_player_collisions(player_1, player_2)
 
         #pauses the game for a second to show who won the round, then resets the round
         if winner == True:
@@ -184,6 +198,7 @@ def main():
             score[0] += 1
             score[1] += 1
             winner = True
+
         #declares player 2 the winner
         elif winner == False and player_2.collision_detect_alt(arena) and not player_1.collision_detect_alt(arena):
             arena.color = palette.red
@@ -214,17 +229,7 @@ def main():
         # Set the screen background
         screen.fill(palette.black)
         #sets up positons for the text objects
-        textRect = text.get_rect()
-        textRect.center = (round(gamevar.screen_width/2),round(gamevar.screen_height/1.5))
-        textRect2 = text2.get_rect()
-        textRect2.center = (50, round(gamevar.screen_height/2))
-        textRect3 = text3.get_rect()
-        textRect3.center = (round(gamevar.screen_width -50), round(gamevar.screen_height/2))
-        screen = draw_arena(screen, arena, gamevar.screen_height, palette)
-        #draws the text onto the screen
-        screen.blit(text, textRect)
-        screen.blit(text2, textRect2)
-        screen.blit(text3, textRect3)
+        screen = draw_text(screen, text, text2, text3, arena)
         # Draw the balls
         screen = draw_flames(ball_list, screen, palette)
 
